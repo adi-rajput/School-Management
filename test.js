a543353a57bcdca9b2cf06328e27e414eb3404e6
ADITYARAJPUT2505 <ar9412349716@gmail.com>
2024-08-24 15:49:19 +0530

    rate limitter tested

---
+++ test.js
@@ -0,0 +1,10 @@
+// http://localhost:8000/api/v1/school/listSchools?latitude=31.3450000&longitude=-74.6501000
+
+// for (let i = 0; i < 1000; i++) {
+//     fetch(
+//       "http://localhost:8000/api/v1/school/listSchools?latitude=31.3450000&longitude=-74.6501000"
+//     ).then((data) => {
+//       console.log(data.json());
+//     });
+//   }
+  
